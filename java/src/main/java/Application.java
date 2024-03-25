e53703bcd26d420224ead2700160e3944291e667
lixiang <lixiang@nasimobi.com>
2024-03-25 13:54:54 +0800

    'java_clinet'

--- java/src/main/java/Application.java
+++ java/src/main/java/Application.java
@@ -1,3 +1,4 @@
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.tensorflow.framework.DataType;
@@ -7,58 +8,95 @@ import tensorflow.serving.Model;
 import tensorflow.serving.Predict;
 import tensorflow.serving.PredictionServiceGrpc;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class Application {
-    public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8500).usePlaintext(true).build();
-        // 这里使用block模式
-        PredictionServiceGrpc.PredictionServiceBlockingStub stub = PredictionServiceGrpc.newBlockingStub(channel);
-        // 创建请求
-        Predict.PredictRequest.Builder predictRequestBuilder = Predict.PredictRequest.newBuilder();
-        // 模型名称和模型方法名预设
-        Model.ModelSpec.Builder modelSpecBuilder = Model.ModelSpec.newBuilder();
-        modelSpecBuilder.setName("ner");
-        modelSpecBuilder.setSignatureName("predict");
-        predictRequestBuilder.setModelSpec(modelSpecBuilder);
-        // 设置入参,访问默认是最新版本，如果需要特定版本可以使用tensorProtoBuilder.setVersionNumber方法
-        List<Float> input = Arrays.asList(13f, 45f, 13f, 13f, 49f, 1f, 49f, 196f, 594f, 905f, 48f, 231f, 318f, 712f, 1003f, 477f, 259f, 291f, 287f, 161f, 65f, 62f, 82f, 68f, 2f, 10f);
+
+    static List<String> schema;
+
+    public static void initSchema(String schemaConf) throws IOException {
+        schema = new ArrayList<>();
+
+        InputStream fileInputStream = Application.class.getClassLoader().getResourceAsStream(schemaConf);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (line.startsWith("#") || line.startsWith("label")) {
+                continue;
+            }
+            String name = line.split(" +")[0];
+            schema.add(name);
+        }
+        System.out.println("schema: " + schema);
+    }
+
+    public static TensorProto buildStringTensorProto(List<String> input) {
         TensorProto.Builder inputTensorProto = TensorProto.newBuilder();
-        inputTensorProto.setDtype(DataType.DT_INT32);
-        inputTensorProto.addAllFloatVal(input);
+        inputTensorProto.setDtype(DataType.DT_STRING);
+        List<ByteString> list = input.stream().map(x -> ByteString.copyFromUtf8(x)).collect(Collectors.toList());
+        inputTensorProto.addAllStringVal(list);
         TensorShapeProto.Builder inputShapeBuilder = TensorShapeProto.newBuilder();
-        inputShapeBuilder.addDim(TensorShapeProto.Dim.newBuilder().setSize(1));
         inputShapeBuilder.addDim(TensorShapeProto.Dim.newBuilder().setSize(input.size()));
         inputTensorProto.setTensorShape(inputShapeBuilder.build());
 
-        int dropout = 1;
-        TensorProto.Builder dropoutTensorProto = TensorProto.newBuilder();
-        dropoutTensorProto.setDtype(DataType.DT_FLOAT);
-        dropoutTensorProto.addIntVal(dropout);
-        TensorShapeProto.Builder dropoutShapeBuilder = TensorShapeProto.newBuilder();
-        dropoutShapeBuilder.addDim(TensorShapeProto.Dim.newBuilder().setSize(1));
-        dropoutTensorProto.setTensorShape(dropoutShapeBuilder.build());
-
-        List<Integer> seqLength = Collections.singletonList(26);
-        TensorProto.Builder seqLengthTensorProto = TensorProto.newBuilder();
-        seqLengthTensorProto.setDtype(DataType.DT_INT32);
-        seqLengthTensorProto.addAllIntVal(seqLength);
-        TensorShapeProto.Builder seqLengthShapeBuilder = TensorShapeProto.newBuilder();
-        seqLengthShapeBuilder.addDim(TensorShapeProto.Dim.newBuilder().setSize(1));
-        seqLengthTensorProto.setTensorShape(seqLengthShapeBuilder.build());
-
-        predictRequestBuilder.putInputs("input", inputTensorProto.build());
-        predictRequestBuilder.putInputs("drop_out", dropoutTensorProto.build());
-        predictRequestBuilder.putInputs("sequence_length", seqLengthTensorProto.build());
-
-        // 访问并获取结果
-        Predict.PredictResponse predictResponse = stub.withDeadlineAfter(3, TimeUnit.SECONDS).predict(predictRequestBuilder.build());
-        Map<String, TensorProto> result = predictResponse.getOutputsMap();
-        // CRF模型结果，发射矩阵和概率矩阵
-        System.out.println("预测值是:" + result.toString());
+        return inputTensorProto.build();
     }
+
+    public static TensorProto buildFloatTensorProto(List<Float> input) {
+        TensorProto.Builder inputTensorProto = TensorProto.newBuilder();
+        inputTensorProto.setDtype(DataType.DT_FLOAT);
+        inputTensorProto.addAllFloatVal(input);
+        TensorShapeProto.Builder inputShapeBuilder = TensorShapeProto.newBuilder();
+        inputShapeBuilder.addDim(TensorShapeProto.Dim.newBuilder().setSize(input.size()));
+        inputTensorProto.setTensorShape(inputShapeBuilder.build());
+
+        return inputTensorProto.build();
+    }
+
+
+    public static PredictionServiceGrpc.PredictionServiceBlockingStub getPredictionServiceBlockingStub(String ip, int port) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(ip, port).usePlaintext(true).build();
+        PredictionServiceGrpc.PredictionServiceBlockingStub stub = PredictionServiceGrpc.newBlockingStub(channel);
+        return stub;
+    }
+
+    public static void main(String[] args) throws IOException {
+        initSchema("schema.conf");
+
+        PredictionServiceGrpc.PredictionServiceBlockingStub stub = getPredictionServiceBlockingStub("60.205.130.26", 8500);
+
+        Predict.PredictRequest.Builder predictRequestBuilder = Predict.PredictRequest.newBuilder();
+        Model.ModelSpec.Builder modelSpecBuilder = Model.ModelSpec.newBuilder();
+        modelSpecBuilder.setName("model");
+        modelSpecBuilder.setSignatureName("pred");
+        predictRequestBuilder.setModelSpec(modelSpecBuilder);
+
+        List<String> input = Arrays.asList("1", "1");
+        for (String name : schema) {
+            predictRequestBuilder.putInputs(name, buildStringTensorProto(input));
+        }
+        List<Float> labels = Arrays.asList((float) 0, (float) 0);
+        predictRequestBuilder.putInputs("label", buildFloatTensorProto(labels));
+
+        for (int i = 0;i < 1e5;i++){
+            long time1 = System.currentTimeMillis();
+            Predict.PredictResponse predictResponse = stub.withDeadlineAfter(3, TimeUnit.SECONDS).predict(predictRequestBuilder.build());
+            Map<String, TensorProto> result = predictResponse.getOutputsMap();
+            List<Float> scores1 = result.get("output1").getFloatValList();
+            long time2 = System.currentTimeMillis();
+
+            System.out.println(result);
+//            System.out.println("scores1: " + scores1 + " time: " + (time2 - time1));
+            break;
+        }
+
+    }
+
+
 }
